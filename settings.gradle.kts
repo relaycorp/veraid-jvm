085460281fe37a5689477ee58470894081ab7408
Gus Narea <gnarea@users.noreply.github.com>
2023-03-17 18:27:58 +0100

    enable build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
 rootProject.name = "veraid"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
+plugins {
+    id("com.gradle.enterprise").version("3.12.4")
+}
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailureIf(!System.getenv("CI").isNullOrEmpty())
+    }
+}
